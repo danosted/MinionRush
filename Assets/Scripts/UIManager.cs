d2e192ba4115c3b4d3695b0b60e7e4e5c934f52c
Christoffer Greulich <cgreulich@gmail.com>
2014-02-01 23:02:38 +0100

    no message

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    private int gooCount;
+    private int goldCount;
+    private int minionCount;
+
+    [SerializeField]
+    private TextMesh gooText;
+    [SerializeField]
+    private TextMesh goldText;
+    [SerializeField]
+    private TextMesh minionText;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        /*// Make a background box
+        GUI.Box(new Rect(10, 10, 100, 90), "Goo Bank");
+        GUI.Box(new Rect(120, 10, 100, 90), "Gold Pile");
+        GUI.Box(new Rect(230, 10, 100, 90), "Gold Pile");
+        
+        GUI.Label(new Rect(10, 30, 80, 60), ""+ gooCount);
+        GUI.Label(new Rect(120, 30, 80, 60), "" + goldCount);
+        GUI.Label(new Rect(230, 30, 80, 60), "" + minionCount);
+         * */
+    }
+
+    public void updateGold(int g)
+    {
+        goldText.text = "Gold Pile: " + g;
+    }
+
+    public void updateGoo(int g)
+    {
+        gooText.text = "Goo Bank: " + g;
+    }
+
+    public void updateMinioncount(int c)
+    {
+        minionText.text = "Minion Count: " + c;
+    }
+}
