a2ff5a75c615f1a59ae4cbadaaaaf99b2639e9a1
Dan Rosenberg Osted <danosted@gmail.com>
2014-02-01 19:39:25 +0100

    Started framework for diff minion spawn, not done tho

---
+++ Assets/Scripts/ScoreManagerScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManagerScript : MonoBehaviour {
+
+	[SerializeField]
+	private int gold;
+	[SerializeField]
+	private int goo;
+	
+	private int highscore;
+	private int minionCount;
+	
+	public void AddGold(int gold)
+	{
+		this.gold += gold;
+	}
+
+	public bool RemoveGold(int gold)
+	{
+		if(this.gold - gold >= 0)
+		{
+			this.gold -= gold;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public void AddGoo(int goo)
+	{
+		this.goo += goo;
+	}
+	
+	public bool RemoveGoo(int gold)
+	{
+		if(this.goo - goo >= 0)
+		{
+			this.goo -= goo;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public void AddMinion(int minions)
+	{
+		this.minionCount += minions;
+	}
+}
