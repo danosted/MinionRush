ba8d8d8d37ebb51eef1ceac72742c983a6b3ad85
Dan Rosenberg Osted <danosted@gmail.com>
2014-02-01 15:55:53 +0100

    intiial

---
+++ Assets/MinionSpawnerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionSpawnerScript : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject minionType1;
+	[SerializeField]
+	private float gooAmount;
+
+	private Clickable clickable;
+	
+	void Start () {
+	
+		clickable = transform.FindChild("Button").GetComponent<Clickable>();
+		clickable.OnClick += SpawnMinion;
+
+	}
+
+	private void SpawnMinion()
+	{
+
+	}
+
+}
---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clickable : MonoBehaviour {
+	
+	public delegate void OnClickDelegate();
+	public event OnClickDelegate OnClick;
+
+	public delegate void OnPressDelegate();
+	public event OnPressDelegate OnPress;
+
+	public delegate void OnExitDelegate();
+	public event OnExitDelegate OnExit;
+	
+	void OnMouseUp()
+	{
+		if(OnClick != null)
+			OnClick();
+	}
+
+	void OnMouseExit()
+	{
+		if(OnExit != null)
+			OnExit();
+	}
+
+	void OnMouseDown()
+	{
+		if(OnPress != null)
+			OnPress();
+	}
+
+	void OnMouseEnter()
+	{
+		if(Input.GetMouseButton(0))
+		{
+			if(OnPress != null)
+				OnPress();
+		}
+	}
+	
+}
