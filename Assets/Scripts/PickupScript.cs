c6adba72169c9475dbdee2d293f71dc8ed6d8e1d
Dan Rosenberg Osted <danosted@gmail.com>
2014-02-01 20:50:43 +0100

    Scoresystem with pickups

---
+++ Assets/Scripts/PickupScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupScript : MonoBehaviour {
+
+	private int gooToAdd;
+	private int goldToAdd;
+
+	//TODO: Inject
+	[SerializeField]
+	private ScoreManagerScript scoreManager;
+
+	void Start()
+	{
+		gooToAdd = GetComponent<CreatureScript>().goo;
+		goldToAdd = GetComponent<CreatureScript>().gold;
+	}
+
+	void OnTriggerEnter2D()
+	{
+		scoreManager.AddGoo(gooToAdd);
+		scoreManager.AddGold(goldToAdd);
+		Destroy(gameObject);
+	}
+
+}
