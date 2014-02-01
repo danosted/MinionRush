f9e134666989d4f1e910fdc772e6bfe712482b46
Christoffer Greulich <cgreulich@gmail.com>
2014-02-01 19:22:01 +0100

    no message

---
+++ Assets/Scripts/FightController.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightController : MonoBehaviour {
+
+    public GameObject attackFX;
+
+    private FightController target;
+
+    private float damage;
+    private float attackCd;
+    private float health;
+
+    private float curAtkCd;
+
+    private bool fighting = false;
+
+    private MovementController moveController;
+    private CreatureScript creatureScript;
+
+	// Use this for initialization
+	void Start () {
+        moveController = transform.GetComponent<MovementController>();
+        creatureScript = transform.GetComponent<CreatureScript>();
+        damage = creatureScript.damage;
+        attackCd = creatureScript.attackCooldown;
+        health = creatureScript.health;
+        curAtkCd = attackCd;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fighting && target)
+        {
+            if (curAtkCd > attackCd)
+            {
+                //got a target an' ready to bash 'em!
+                float targetHealth = target.ApplyDamage(damage);
+                GameObject.Instantiate(attackFX, target.transform.position, Quaternion.identity);
+                curAtkCd = 0;
+                if (targetHealth > 0)
+                {
+                    //keep fighting this guy
+                }
+                else
+                {
+                    //he's dead, get the next one!
+                    SetFighting(false);
+                }
+            }
+            else
+            {
+                //got a target, but not ready to bash
+                curAtkCd += Time.deltaTime;
+            }
+        }
+        else
+        {
+            SetFighting(false);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(gameObject + " hit a collider with name " + col.gameObject);
+        if (col.gameObject.tag == "Ground")
+        {
+
+        }
+        else
+        {
+            target = col.transform.GetComponent<FightController>();
+            moveController.SetMoving(false);
+            fighting = true;
+        }
+    }
+
+    float ApplyDamage(float dam)
+    {
+        health -= dam;
+        if(health > 0) {
+            //still alive bitches!
+        } else
+        {
+          //yargh!
+            Destroy(gameObject);
+            if (creatureScript)
+            {
+                creatureScript.Die();
+            }
+        }
+        return health;
+    }
+
+    public void SetFighting(bool b)
+    {
+        fighting = b;
+        moveController.SetMoving(!b);
+        if (b)
+        {
+            target = null;
+        }
+    }
+}
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -13,7 +13,7 @@ public class MovementController : MonoBehaviour {
     private CreatureScript creatureScript;
     private CharacterController charController;
 
-    private CollisionFlags collisionFlags;
+    private bool shouldMove = true;
 
 	// Use this for initialization
 	void Start () {
@@ -25,7 +25,11 @@ public class MovementController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Move(movespeed);
+        if (shouldMove)
+        {
+            //Debug.Log(gameObject + "is moving");
+            Move(movespeed);
+        }
 	    
 	}
 
@@ -34,5 +38,11 @@ public class MovementController : MonoBehaviour {
         movement *= Time.deltaTime;
         transform.position += new Vector3(movement, 0, 0);
         //collisionFlags = CharacterController.Move(new Vector3(0, movement,0));
+    }
+
+    public void SetMoving(bool s)
+    {
+       //new WaitForSeconds(Random.Range(0.1f, 0.5f));
+        shouldMove = s;
     }
 }
